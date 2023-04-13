344f4cf1846e50e11146855d49d1bab4a67c618a
Juan Esteban Caicedo Alzate <61593226+TheLordJuanes@users.noreply.github.com>
2023-04-12 20:43:35 -0500

    Test containers setup (#4)

--- src/test/java/com/shelter/animalback/integration/db/animal/AnimalDataBaseTest.java
+++ src/test/java/com/shelter/animalback/integration/db/animal/AnimalDataBaseTest.java
@@ -5,7 +5,11 @@ import com.shelter.animalback.repository.AnimalRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
@@ -13,10 +17,21 @@ import static org.hamcrest.Matchers.notNullValue;
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         properties = { "spring.config.additional-location=classpath:integration-test.yml"})
+@Testcontainers
 public class AnimalDataBaseTest {
+
+    @Container
+    private static PostgreSQLContainer database = new PostgreSQLContainer("postgres:13.2");
 
     @Autowired
     private AnimalRepository animalRepository;
+
+    @DynamicPropertySource
+    static void databaseProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", database::getJdbcUrl);
+        registry.add("spring.datasource.username", database::getUsername);
+        registry.add("spring.datasource.password", database::getPassword);
+    }
 
     @Test
     public void createAnimal() {
